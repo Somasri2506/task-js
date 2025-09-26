2ba81e08f65dea677cb68fa06c357ade0c2d25a4
Somasri2506 <somasri7300@gmail.com>
2025-09-26 11:15:26 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,15 @@
+let num="85";
+let num1=Number(num);
+console.log(num1);
+if(num1>=90&&num1>=100){
+    console.log("Grade: A");
+}
+else if(num1>=75&&num1<=89){
+    console.log("Grade: B");
+}
+else if(num1>50&&num1<=74){
+    console.log("Grade: C");
+}
+else{
+    console.log("Fail");
+}
---
+++ task2.js
@@ -0,0 +1,21 @@
+let val="12";
+let val1=Number(val);
+if(val1%2==0){
+    console.log("Even");
+
+}
+else{
+    console.log("Odd");
+
+}
+let day=4;
+if(day>=1&&day<=5){
+    console.log("It's a weekday");
+}
+else if(day==6||day==7){
+    console.log("It's a weekend");
+
+}
+else{
+    console.log("Invalid day");
+}
